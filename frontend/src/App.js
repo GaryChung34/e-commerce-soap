de92575750f99e08b9965c8f7ee4b44aded206cb
gary-home <gary64800@gmail.com>
2021-08-02 23:18:28 +0800

    commit for adding home page to react app.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,56 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="header">
+        <div className="brand">
+          <a href="">Simple Soap</a>
+        </div>
+        <div className="header-link">
+          <a href="">Cart</a>
+          <a href="">Log in</a>
+        </div>
       </header>
-    </div>
+
+    <main className="main">
+      <img className="banner" src="images/logo-center.jpg" alt="logo-wide.jpg" />
+      <div className="button-bar">
+        <button className="button blue-button">About us</button>
+        <button className="button blue-button">Products</button>
+        <button className="button blue-button">Gallery</button>
+        <button className="button blue-button">Blog</button>
+      </div>
+
+      <h1 className="all-products">
+        All Produsts
+      </h1>
+
+      <div className="all-products">
+        <div className="product">
+          <img className="product-image" src="images/soap1.jpg" alt="soap1" />
+          <h2>Soap 1</h2>
+          <div>$ 45.00</div>
+        </div>
+        <div className="product">
+          <img className="product-image" src="images/soap2.png" alt="soap2" />
+          <h2>Soap 2</h2>
+          <div>$ 60.00</div>
+        </div>
+        <div className="product">
+          <img className="product-image" src="images/soap3.jpg" alt="soap3" />
+          <h2>Soap 3</h2>
+          <div>$ 70.00</div>
+        </div>
+        <div className="product">
+          <img className="product-image" src="images/soap4.jpg" alt="soap4" />
+          <h2>Soap 4</h2>
+          <div>$ 50.00</div>
+        </div>
+      </div>
+    </main>
+
+    <footer className="footer">footer</footer>
+  </div>
   );
 }
 
