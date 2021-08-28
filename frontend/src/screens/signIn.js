1346c99116fe35b8b630073835892b36e8682bde
gary-home <gary64800@gmail.com>
2021-08-28 22:49:13 +0800

    sign in page with react-redux side function.

--- frontend/src/screens/signIn.js
+++ frontend/src/screens/signIn.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+// import { userLogin } from '../feature/user/userSlice.js'
 
 
 const SignIn = () => {
+	const [ user, setUser ] = useState('')
+	const [ password, setPassword ] = useState('')
+	// const userSignin = useSelector(state => state.user)
+	const dispatch = useDispatch()
+
+	const onUserChange = (e) => {
+		setUser(e.target.value)
+	}
+
+	const onPasswordChange = (e) => {
+		setPassword(e.target.value)
+	}
+
+	const handleSubmit = () => {
+		// dispatch(userLogin)
+	}
+
+	// useEffect({
+	// 	if(userLogin) {
+	// 		this.history.push('/')
+	// 	}
+	// }, [userSignin]) 
+
+
 	return(
 		<div>
 			<h1>Sign-in </h1>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<label>User:</label>
-				<input type="text" />
+				<input type="text" id='user' name='user'
+					onChange={onUserChange}/>
 				<label>Password:</label>
-				<input type="text" />
+				<input type="text" id='password' name='password'
+					onChange={onPasswordChange}/>
 			</form>
 		</div>
 	)
