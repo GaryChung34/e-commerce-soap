5a30ccd08a476c62777a68c7a04bb0cc96eaa715
gary-home <gary64800@gmail.com>
2021-09-04 23:47:21 +0800

    working on manage item edit form.

--- frontend/src/screens/manageItem.js
+++ frontend/src/screens/manageItem.js
@@ -6,6 +6,7 @@ import { addProduct } from '../feature/products/productsSlice.js'
 const ManageItem = (props) => {
 	const products = useSelector(state => state.products.products)
 
+	const [ id, setId ] = useState('')
 	const [ name, setName ] = useState('') 
 	const [ star, setStar ] = useState('') 
 	const [ image, setImage ] = useState('') 
@@ -14,9 +15,28 @@ const ManageItem = (props) => {
 	const [ ingredient1, setIngredient1 ] = useState('') 
 	const [ ingredient2, setIngredient2 ] = useState('') 
 	const [ ingredient3, setIngredient3 ] = useState('') 
+	const [ modelVisible , setModelVisible ] = useState(false)
 
 	const dispatch = useDispatch()
 
+	const openModel = (product) => {
+		setModelVisible(true)
+		setId(product._id)
+		setName(product.name)
+		setStar(product.star)
+		setImage(product.image)
+		setOrigin(product.price.origin)
+		setDiscount(product.price.discount)
+		if(product.ingredients[0]){
+			setIngredient1(product.ingredients[0])
+		}
+		if(product.ingredients[1]){
+			setIngredient2(product.ingredients[1])
+		}
+		if(product.ingredients[2]){
+			setIngredient3(product.ingredients[2])
+		}
+	}
 
 	const handleSubmit = (e) => {
 		e.preventDefault()
@@ -36,59 +56,62 @@ const ManageItem = (props) => {
 	return (
 		<div>
 			<h1>Manage Item</h1>
-			<form onSubmit={handleSubmit}>
-				<ul>
-					<li>
-						<label>name:</label>
-					</li>
-					<li>
-						<input type='text' onChange={(e)=>{setName(e.target.value)}} />
-					</li>
-					<li>
-						<label>star:</label>
-					</li>
-					<li>
-						<input type='number' onChange={(e)=>{setStar(e.target.value)}} />
-					</li>
-					<li>
-						<label>image:</label>
-					</li>
-					<li>
-						<input type='text' onChange={(e)=>{setImage(e.target.value)}} />
-					</li>
-					<li>
-						<label>Original price:</label>
-					</li>
-					<li>
-						<input type='text' onChange={(e)=>{setOrigin(e.target.value)}} />
-					</li>
-					<li>
-						<label>Discount price:</label>
-					</li>
-					<li>
-						<input type='text' onChange={(e)=>{setDiscount(e.target.value)}} />
-					</li>
-					<li>
-						<label>ingredient 1:</label>
-					</li>
-					<li>
-						<input type='text' onChange={(e)=>{setIngredient1(e.target.value)}} />
-					</li>
-					<li>
-						<label>ingredient 2:</label>
-					</li>
-					<li>
-						<input type='text' onChange={(e)=>{setIngredient2(e.target.value)}} />
-					</li>
-					<li>
-						<label>ingredient 3:</label>
-					</li>
-					<li>
-						<input type='text' onChange={(e)=>{setIngredient3(e.target.value)}} />
-					</li>
-					<button type='submit'>Submit</button>
-				</ul>
-			</form>
+			{modelVisible &&
+				<form onSubmit={handleSubmit}>
+					<ul>
+						<li>
+							<label>name:</label>
+						</li>
+						<li>
+							<input type='text' onChange={(e)=>{setName(e.target.value)}} value={name}/>
+						</li>
+						<li>
+							<label>star:</label>
+						</li>
+						<li>
+							<input type='number' onChange={(e)=>{setStar(e.target.value)}} value={star}/>
+						</li>
+						<li>
+							<label>image:</label>
+						</li>
+						<li>
+							<input type='text' onChange={(e)=>{setImage(e.target.value)}} value={image}/>
+						</li>
+						<li>
+							<label>Original price:</label>
+						</li>
+						<li>
+							<input type='text' onChange={(e)=>{setOrigin(e.target.value)}} value={origin}/>
+						</li>
+						<li>
+							<label>Discount price:</label>
+						</li>
+						<li>
+							<input type='text' onChange={(e)=>{setDiscount(e.target.value)}} value={discount}/>
+						</li>
+						<li>
+							<label>ingredient 1:</label>
+						</li>
+						<li>
+							<input type='text' onChange={(e)=>{setIngredient1(e.target.value)}} value={ingredient1}/>
+						</li>
+						<li>
+							<label>ingredient 2:</label>
+						</li>
+						<li>
+							<input type='text' onChange={(e)=>{setIngredient2(e.target.value)}} value={ingredient2}/>
+						</li>
+						<li>
+							<label>ingredient 3:</label>
+						</li>
+						<li>
+							<input type='text' onChange={(e)=>{setIngredient3(e.target.value)}} value={ingredient3}/>
+						</li>
+						<button type='submit'>Submit</button>
+						<button onClick={() => {setModelVisible(false)}}>Back</button>
+					</ul>
+				</form>
+			}
 			<table>
 				<thead>
 					<th>id</th>
@@ -107,7 +130,7 @@ const ManageItem = (props) => {
 							<td>{product.price.origin}</td>
 							<td>{product.price.discount}</td>
 							<td>
-								<button>edit</button>
+								<button onClick={() => openModel(product)}>edit</button>
 								<button>delete</button>
 							</td>
 						</tr>
