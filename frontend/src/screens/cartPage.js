247cdd79923e12a693f1db82691f1f9ae1d07df8
gary-home <gary64800@gmail.com>
2021-09-08 23:06:06 +0800

    working on place order page.

--- frontend/src/screens/cartPage.js
+++ frontend/src/screens/cartPage.js
@@ -4,6 +4,15 @@ import { addCart } from '../feature/cart/cartSlice.js'
 
 import CartItem from '../components/cartItem.js'
 
+const cartTotalPrice = (cartItems, products) => {
+		let result = 0
+		cartItems.map(item => {
+			const cartItem = products.find(product => product._id === item.id)
+			result += parseFloat(cartItem.price.discount) * item.quantity
+		})
+
+		return result
+	}
 
 const CartPage = (props) => {
 	const cartItems = useSelector(state => state.cart.cart)
@@ -19,17 +28,6 @@ const CartPage = (props) => {
 		)
 	})
 
-	const total = () => {
-		let result = 0
-		cartItems.map(item => {
-			const cartItem = products.find(product => product._id === item.id)
-			console.log(cartItem)
-			result += parseFloat(cartItem.price.discount) * item.quantity
-		})
-
-		return result
-	}
-
 	const handleCheckout = () => {
 		props.history.push('/signIn?redirect=/shipping')
 	}
@@ -38,10 +36,11 @@ const CartPage = (props) => {
 		<div className='cartList'>
 			<h1>Cart: </h1>
 			{cartRender}
-			<h2>Total:&nbsp;${total()}</h2>
+			<h2>Total:&nbsp;${cartTotalPrice(cartItems, products)}</h2>
 			<button onClick={handleCheckout}>Checkout</button>
 		</div>
 	)
 }
 
 export default CartPage
+export { cartTotalPrice }
--- frontend/src/screens/placeOrder.js
+++ frontend/src/screens/placeOrder.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import CartItem from '../components/cartItem.js'
+import { cartTotalPrice } from './cartPage.js'
+
 
 const PlaceOrder = () => {
+	const shipping = useSelector(state => state.cart.shipping)
+	const { paymentMethod } = useSelector(state => state.cart.payment)
+	const cartItems = useSelector(state => state.cart.cart)
+	const products = useSelector(state => state.products.products)
+	const [ deliveryFee, setDeliveryFee ] = useState('10')
+
+	const cartRender = cartItems.map(item => {
+		const cartItem = products.find(product => product._id === item.id)
+
+		return(
+			<CartItem product={cartItem} qty={item.quantity} />
+		)
+	})
 
 	return (
 		<div>
 			<h1>Place Order:</h1>
+			<h2>Shipping Address:</h2>
+			<div>
+			{shipping.address}, 
+			{shipping.city}, 
+			{shipping.country}
+			</div>
+
+			<h2>Payment Method:</h2>
+			<div>{paymentMethod}</div>
+
+			<h2>Order Item:</h2>
+			{cartRender}
+
+			<h2>Order Summary:</h2>
+			<div>Items: ${cartTotalPrice(cartItems, products)}</div>
+			<div>Delivery fee: ${deliveryFee}</div>
+
 		</div>
 	)
 }
