7c47b25ca7df255acb8dfd63370952d416bd9a7a
Gary Chung <gary64800@gmail.com>
2021-09-21 12:34:26 +0800

    version 1.0.0

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import config from './config.js'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
+import path from 'path'
 
 import usersRoute from './routes/usersRoute.js'
 import productRoute from './routes/productRoute.js'
@@ -31,6 +32,18 @@ app.get('/api/paypal', (req, res) => {
 	console.log(config.PAYPAL_CLIENT_ID)
 	res.send({clientId: config.PAYPAL_CLIENT_ID})
 })
+
+// serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+
+	//use static file
+	app.use(express.static('frontend/build'))
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+	})
+}
+
 
 app.listen('2000', () => {
 	console.log('this service is hold on port: 2000')
